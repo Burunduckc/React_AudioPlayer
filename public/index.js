1eef9375bf5f059904005fcf4212912a73f73217
Burunduckc <1999Melkymov@gmail.com>
2023-08-13 00:27:03 +0300

    табы

--- public/index.js
+++ public/index.js
@@ -7,7 +7,6 @@ oneBlock.forEach((a, i) => {
         twoBlock.forEach((b) => b.classList.remove('opacity'))
 
     a.classList.add('active')
-    twoBlock[i].classList.add('active')
     twoBlock[1 - i].classList.add('opacity')
     })
 })
